92efa20223ae2ec9abed83bc201b909f32abed14
Denis345 <51526615+Denis345@users.noreply.github.com>
2025-12-04 15:53:15 +0300

    Add files via upload

---
+++ funnyrantoke/funnyrantoke/README.txt
@@ -0,0 +1,7 @@
+{"msg": "succeeded", "keys": {"active_key":
+ {"public": "EOS7a1j6JfsUt64LTuKRfTjgHBUQ5sTbvf3hEhL97gMdEakqZKj4y",
+  "private": "5JYAM4FvhpEokpc8iYDwoyNjRsF7HdmiRghm9pDiFSV6raNJata"},
+   "owner_key": {"public":
+    "EOS8MKni79uSo1FWXW1MmYi1hrktbo3B2sPV1UvR55Hj1JPKkn8Td", 
+    "private": "5KAECHKCMAQCeqqVaub7zepCJGmSyaqad98z2uG5mCfKsbx2Hf8"}},
+     "account": "funnyrantoke"}
---
+++ funnyrantoke/funnyrantoke/build/CMakeFiles/funnyrantoke_project.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/funnyrantoke_project"
+  "CMakeFiles/funnyrantoke_project-complete"
+  "funnyrantoke_project-prefix/src/funnyrantoke_project-stamp/funnyrantoke_project-install"
+  "funnyrantoke_project-prefix/src/funnyrantoke_project-stamp/funnyrantoke_project-mkdir"
+  "funnyrantoke_project-prefix/src/funnyrantoke_project-stamp/funnyrantoke_project-download"
+  "funnyrantoke_project-prefix/src/funnyrantoke_project-stamp/funnyrantoke_project-update"
+  "funnyrantoke_project-prefix/src/funnyrantoke_project-stamp/funnyrantoke_project-patch"
+  "funnyrantoke_project-prefix/src/funnyrantoke_project-stamp/funnyrantoke_project-configure"
+  "funnyrantoke_project-prefix/src/funnyrantoke_project-stamp/funnyrantoke_project-build"
+  "funnyrantoke_project-prefix/src/funnyrantoke_project-stamp/funnyrantoke_project-test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/funnyrantoke_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
